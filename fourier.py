79e061b63f9de997b5d903c44a01d87561fffbe2
iwl-lyam <64089164+iwl-lyam@users.noreply.github.com>
2023-05-29 14:43:08 +0100

    Finding notes

--- fourier.py
+++ fourier.py
@@ -1,16 +1,17 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import sounddevice as sd
 from scipy.signal import find_peaks
+import matplotlib.pyplot as plt
 
-T = 3
+duration = float(input("Enter the recording duration in seconds: "))
+
 fs = 44100
-N = int(T * fs)
+N = int(duration * fs)
 
 input("Press enter to start recording")
 
 print("Recording started.")
-audio = sd.rec(int(T * fs), samplerate=fs, channels=1)
+audio = sd.rec(N, samplerate=fs, channels=1)
 sd.wait()
 print("Recording finished.")
 
@@ -30,14 +31,70 @@ print("Detect peaks")
 peaks, _ = find_peaks(np.abs(F))
 
 peak_frequencies = omega[peaks]
-positive_peak_frequencies = peak_frequencies[peak_frequencies > 0]
+peak_amplitudes = np.abs(F)[peaks]
 
-print("Creating output audio", len(positive_peak_frequencies))
+positive_peak_frequencies = peak_frequencies[peak_frequencies > 0]
+positive_peak_amplitudes = peak_amplitudes[peak_frequencies > 0]
+
+played_frequencies = []
+note_count = {}
+
+print("Positive peak frequencies and musical notes:")
+for freq, amp in zip(positive_peak_frequencies, positive_peak_amplitudes):
+    if freq > 7902.13 or amp < 0.00004:  # Limit to B8 (7902.13 Hz) and minimum amplitude
+        continue
+    
+    played_frequencies.append(freq)
+    if freq / 2 in played_frequencies:
+        continue
+    
+    note = 69 + 12 * np.log2(freq / 440)
+    note_name = round(note) % 12
+    note_name_dict = {
+        0: 'C',
+        1: 'C#',
+        2: 'D',
+        3: 'D#',
+        4: 'E',
+        5: 'F',
+        6: 'F#',
+        7: 'G',
+        8: 'G#',
+        9: 'A',
+        10: 'A#',
+        11: 'B'
+    }
+    octave = int((round(note) - 12) / 12)
+    note_string = note_name_dict[note_name] + str(octave)
+    print("{:.2f} Hz - {}".format(freq, note_string))
+    
+    # Update note count
+    if note_string in note_count:
+        note_count[note_string] += amp
+    else:
+        note_count[note_string] = amp
+
+print("Note Count:")
+for note, count in note_count.items():
+    print("{}: {:.4f}".format(note, count))
+
+# Create bar graph
+notes = list(note_count.keys())
+counts = list(note_count.values())
+
+plt.bar(notes, counts)
+plt.xlabel('Musical Note')
+plt.ylabel('Count')
+plt.title('Note Count')
+plt.grid(True)
+plt.show()
+
+print("Creating output audio", len(played_frequencies))
 duration = len(audio) / fs
 
 composite_tone = np.zeros_like(audio, dtype=float)
 
-for freq in positive_peak_frequencies:
+for freq in played_frequencies:
     tone_duration = 0.001  # Tone duration of 1 ms
     num_samples = int(tone_duration * fs)
     tone = np.sin(2 * np.pi * freq * np.arange(num_samples) / fs)
