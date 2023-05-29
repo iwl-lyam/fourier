85839cca1870c704a0e3501758510de3967078ea
iwl-lyam <64089164+iwl-lyam@users.noreply.github.com>
2023-05-29 09:23:43 +0100

    Fixed output in omegas

--- fourier.py
+++ fourier.py
@@ -4,7 +4,7 @@ import sounddevice as sd
 from scipy.signal import find_peaks
 
 T = 3
-fs = 44100 
+fs = 44100
 N = int(T * fs)
 
 input("Press enter to start recording")
@@ -23,7 +23,8 @@ print("Apply Fourier Transform")
 F = np.fft.fftshift(np.fft.fft(np.fft.fftshift(audio))) / N
 
 print("Find frequencies")
-omega = 2 * np.pi * np.fft.fftshift(np.fft.fftfreq(N, 1/fs))
+freqs = np.fft.fftshift(np.fft.fftfreq(N, 1/fs))
+omega = 2 * np.pi * freqs
 
 print("Detect peaks")
 peaks, _ = find_peaks(np.abs(F))
@@ -32,33 +33,30 @@ peak_frequencies = omega[peaks]
 positive_peak_frequencies = peak_frequencies[peak_frequencies > 0]
 
 print("Creating output audio", len(positive_peak_frequencies))
-duration = len(audio) / fs  
+duration = len(audio) / fs
 
-chunk_size = 1000  # Number of samples per chunk
-composite_tone = np.zeros(len(audio))
+composite_tone = np.zeros_like(audio, dtype=float)
 
-# Generate composite tone in smaller chunks
 for freq in positive_peak_frequencies:
-    tone_chunk = np.sin(2 * np.pi * freq * np.arange(chunk_size) / fs)
-    tone_chunk /= np.max(np.abs(tone_chunk))
-    for i in range(int(len(audio) / chunk_size)):
-        start_idx = i * chunk_size
-        end_idx = (i + 1) * chunk_size
-        composite_tone[start_idx:end_idx] += tone_chunk[:end_idx-start_idx]
+    tone_duration = 0.001  # Tone duration of 1 ms
+    num_samples = int(tone_duration * fs)
+    tone = np.sin(2 * np.pi * freq * np.arange(num_samples) / fs)
+    tone /= np.max(np.abs(tone))
+    composite_tone[:num_samples] += tone
 
 composite_tone /= np.max(np.abs(composite_tone))
 
 input("Press enter to play audio")
-print("Playing recorded audio...")
-sd.play(np.vstack((audio, composite_tone[:len(audio)])).T, samplerate=fs)
+print("Playing recorded audio and composite tone...")
+sd.play(np.column_stack((audio, composite_tone)), samplerate=fs)
 sd.wait()
 
 print("Analyzing audio")
 print("Plotting audio onto a chart")
 
-plt.plot(omega, np.abs(F))
-plt.plot(omega[peaks], np.abs(F)[peaks], 'ro') 
-plt.xlabel('Angular Frequency (omega)')
+plt.plot(freqs, np.abs(F))
+plt.plot(freqs[peaks], np.abs(F)[peaks], 'ro') 
+plt.xlabel('Frequency (Hz)')
 plt.ylabel('Amplitude')
 plt.title('Real-time Fourier Transform')
 plt.grid(True)
